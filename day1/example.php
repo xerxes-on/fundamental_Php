d96b33c6170901a7373eb51557362239e71dd716
xerxes <odilbekovjavohir4@gmail.com>
2024-07-23 15:36:08 +0500

    init

--- day1/example.php
+++ day1/example.php
@@ -1,8 +1,10 @@
 <?php
-$a = 0;
-echo "HI";
+function fib($a){
+    $res = [0,1];
+    for($i=0;$i<=$a-2; $i++){
+        $res[] = $res[count($res)-1] + $res[count($res)-2];
+    }
+    return $res;
+}
 
-
-
-
-?>
+print_r(fib(10));
