cbb3d1f83daea6cf33a4994d17d0c9c7b650f07c
xerxes <odilbekovjavohir4@gmail.com>
2024-07-25 21:25:43 +0500

    fixed fizzBuzz changed == to  ===

--- day1/fizzBuzz.php
+++ day1/fizzBuzz.php
@@ -2,12 +2,12 @@
 function fizzBuzz($a, $b)
 {
     for($i = $a; $i <= $b; $i++){
-        if($i % 3 == 0 && $i % 5 == 0){
+        if($i % 3 === 0 && $i % 5 === 0){
             echo " FizzBuzz ";
         }
-        elseif($i % 3 == 0){
+        elseif($i % 3 === 0){
             echo " Fizz ";
-        }elseif ($i % 5 ==0){
+        }elseif ($i % 5 === 0){
             echo " Buzz ";
         }else{
             echo $i." ";
