4ddb31bfd9394867569bd54d98a23f83080047ed
xerxes <odilbekovjavohir4@gmail.com>
2024-07-25 21:03:55 +0500

    fixed getMirrirMatrix

--- day1/getMirrorMatrix.php
+++ day1/getMirrorMatrix.php
@@ -1,18 +1,20 @@
 <?php
-function getMirrorMatrix($m){
+function getMirrorMatrix($m): array
+{
+    $final_matrix =[];
     foreach($m as $matrix){
         $j = count($matrix)/2;
         for($i = count($matrix)/2 - 1; $i >=0; $i--){
             $matrix[$j] = $matrix[$i];
             $j++;
         }
-        print_r($matrix);
+        $final_matrix[] = $matrix;
     }
-    return $m;
+    return $final_matrix;
 }
-getMirrorMatrix([
+(getMirrorMatrix([
   [11, 12, 13, 14, 15, 16],
   [21, 22, 23, 24, 25, 26],
   [31, 32, 33, 34, 35, 36],
   [41, 42, 43, 44, 45, 46],
-]);
+]));
