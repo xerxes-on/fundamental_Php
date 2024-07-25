98a175dacab3f03baaab03a63bfc9fe30dd29ea1
xerxes <odilbekovjavohir4@gmail.com>
2024-07-25 21:36:43 +0500

    Fibanaci changed used recrusion

--- day1/fibanaci.php
+++ day1/fibanaci.php
@@ -1,9 +1,8 @@
 <?php
-function fib($a){
-    $res = [0,1];
-    for($i=0;$i<=$a-2; $i++){
-        $res[] = $res[count($res)-1] + $res[count($res)-2];
+function fib($n) {
+    if ($n <= 1) {
+        return $n;
     }
-    return $res;
+    return fib($n - 1) + fib($n - 2);
 }
-print_r(fib(10));
+print_r(fib(3));
