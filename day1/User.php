7b66cabfb4a306465fa87dc0e1eee14b598aed93
xerxes <odilbekovjavohir4@gmail.com>
2024-07-26 11:38:39 +0500

    User Class modified

--- day1/User.php
+++ day1/User.php
@@ -8,19 +8,31 @@ class User
     public function setFirstName($name)
     {
         $this->firstName = $name;
+        return $this;
     }
     public function setLastName($last)
     {
         $this->lastName = $last;
+        return $this;
     }
         public function setEmail($email)
     {
         $this->email = $email;
+        return $this;
     }
-
+        public function __toString()
+    {
+        return $this->firstName . ' ' . $this->lastName . "< " . $this->email . '>';
+    }
 
 }
 $user = new User();
-$user->setFirstName('John');
-$user->setLastName('Doe');
-$user->setEmail('john.doe@example.com');
+$user->setFirstName('John')
+    ->setLastName('Doe')
+    ->setEmail('john.doe@example.com');
+echo $user;
+
+
+
+
+
